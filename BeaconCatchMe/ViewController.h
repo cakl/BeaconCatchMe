6014662968efb06d270aa488f2b9c4d0b1c6a8a4
Mario Meili <mario.meili@magnet.ch>
2014-09-23 15:23:58 +0200

    Initial Commit

---
+++ BeaconCatchMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconCatchMe
+//
+//  Created by Mario Meili on 23/09/14.
+//  Copyright (c) 2014 mariomeili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeaconCatchMe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BeaconCatchMe
+//
+//  Created by Mario Meili on 23/09/14.
+//  Copyright (c) 2014 mariomeili. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
